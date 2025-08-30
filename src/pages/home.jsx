5032982466a6f62ba792e58e56c7d25d400e9f9a
somitgarhewal <somitgarhewal@gmail.com>
2025-08-30 20:25:22 +0530

    created progress bar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import ProgressBar from "../components/progressBar";
+
 function Home() {
+  const [completedPercent, setCompletedPercent] = useState(0);
+  const timeToCompleteInterval = 3; // seconds
+  useEffect(() => {
+    const completedPercentInterval =  setInterval(() => {
+        setCompletedPercent((prev) => {
+            if(prev <= 99){
+                return prev + 1;
+            }else{
+                clearInterval(completedPercentInterval);
+                return 100;
+            }
+        })
+    }, (timeToCompleteInterval * 1000) / 100 );
+    return() => {
+        clearInterval(completedPercentInterval);
+    }
+  }, []);
+
   return (
-    <div className="text-red-500">
-        Namaste Duniya
-    </div>
-  )
+    <>
+      <div className="text-red-500">Namaste Duniya</div>
+      <ProgressBar completedPercent={completedPercent} />
+    </>
+  );
 }
 
-export default Home
+export default Home;
