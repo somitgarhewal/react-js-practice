ca01c4e4747983d5fe5301741aa0e3a7b3a47951
somitgarhewal <somitgarhewal@gmail.com>
2025-09-18 13:34:04 +0530

    created scroll pagination

---
+++ src/pages/scrollPagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ScrollPaginationComp from '../components/scrollPaginationComp'
+
+function ScrollPagination() {
+  return (
+    <div>
+      <ScrollPaginationComp />
+    </div>
+  )
+}
+
+export default ScrollPagination
